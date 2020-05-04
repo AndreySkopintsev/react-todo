23bd9575b8cd3a1944dbb1681cb37984da83423d
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2020-05-04 11:20:17 +0300

    Updates

---
+++ precache-manifest.9f5911ce8d914223567476391e0241f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82facabbbfdbc06a0f17d72dfe02420f",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "c8b6dfb712cdeadc9f72",
+    "url": "/react-todo/static/css/main.ccd17487.chunk.css"
+  },
+  {
+    "revision": "f0cfd47854b67d2b7c83",
+    "url": "/react-todo/static/js/2.cdd9ec3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.cdd9ec3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b6dfb712cdeadc9f72",
+    "url": "/react-todo/static/js/main.b9c5a567.chunk.js"
+  },
+  {
+    "revision": "c0917e0e097170cd6617",
+    "url": "/react-todo/static/js/runtime-main.baf36893.js"
+  }
+]);
